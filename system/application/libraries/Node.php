0ac1146680d6b7a5b33c1063ec10ebd0ecdd200f
Skander Jabouzi <jabouzi@gmail.com>
2009-11-13 04:29:30 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/3b/trunk@45 1045d7f8-9d8e-4f2c-ac63-a42effcbc9fb

---
+++ system/application/libraries/Node.php
@@ -0,0 +1,83 @@
+<?php
+ 
+/** 
+* Title: Tree Node Structure
+* Description: a tree node strcuture in PHP 
+* @author Skander Jabouzi www.skanderjabouzi.com
+* @version 0.1 November 2009
+*/
+ 
+class Node
+{
+    private $type;
+    
+    private $data;    
+        
+    private $depth; 
+
+    private $children; 
+   
+
+    function __construct($type,$data,$depth)
+    {
+        $this->type = $type;
+        $this->data = $data;
+        $this->depth = $depth;
+        $this->children = array();
+    }   
+    
+    function getType()
+    {
+        return $this->type;
+    } 
+    
+    function getData()
+    {
+        return $this->data;
+    }  
+    
+    function getDepth()
+    {
+        return $this->depth;
+    }
+    
+    function getChildren()
+    {
+        return $this->children;
+    }
+    
+    function addChild($child)
+    {
+        if (!$this->getChild($child->getType(),$child->getData()))
+        {
+            $this->children[] = $child;
+        }
+        return count($this->children) - 1;
+    }  
+    
+    function getChild($type,$data)
+    {
+        $childFound = false;
+        foreach($this->children as $child)
+        {
+            if ($type == $child->type && $data == $child->data)
+            {
+                $childFound = $child;
+                break;
+            }            
+        }
+        return $childFound;
+    }
+    
+    function hasChildren()
+    {
+        return count($this->getChildren()) > 0;
+    }
+    
+    function getChildAt($position)
+    {
+        $children = $this->getChildren();
+        return $children[$position];
+    }
+}  
+
---
+++ system/application/libraries/Tree.php
@@ -0,0 +1,132 @@
+<?
+
+/** 
+* Title: N-Tree Structure
+* Description: Create multi-level tree strcuture in PHP 
+* @author Skander Jabouzi www.skanderjabouzi.com
+* @version 0.1 November 2009
+*/
+
+require_once("Node.php");
+
+class Tree
+{
+    
+    private $root;
+ 
+    private $count;
+    
+    private $depth;
+    
+    private $childFound;
+    
+    private $childsByDepth;
+ 
+    function __construct()
+    {
+        $this->root = NULL;
+        $this->count = 0;
+        $this->setDepth(0);
+        $this->childFound = false;
+        $this->childsByDepth = array();
+    }
+ 
+    public function isEmpty()
+    {
+        return (NULL == $this->root);
+    } 
+    
+    function addRoot($type,$data)
+    {
+        $node = new Node($type,$data,0);
+        $this->root = $node;
+        $this->incrementCount();
+    }    
+    
+    function getRoot()
+    {
+        return $this->root;
+    }
+    
+    function getCount()
+    {
+        return $this->count;
+    }
+    
+    function getDepth()
+    {
+        return $this->depth;
+    }
+    
+    function getChildFound()
+    {
+        return $this->childFound;
+    }    
+    
+    function getChildsByDepth()
+    {
+        return $this->childsByDepth;
+    }    
+    
+    function incrementCount()
+    {
+        $this->count = $this->count + 1;
+    }   
+    
+    function setDepth($newDepth)
+    {
+        $this->depth = $newDepth;
+    }
+    
+    
+    function insertRootChild($type,$data)
+    {
+        $node = new Node($type,$data,1);         
+        $this->incrementCount();   
+        return $this->root->addChild($node);    
+    }
+    
+    function insertChild($type,$data,$node)
+    {
+        $childDepth = $node->getDepth() + 1;
+        $child = new Node($type,$data,$childDepth);         
+        $this->incrementCount();     
+        if ($childDepth > $this->getDepth())
+        {
+            $this->setDepth($childDepth);
+        }
+        return $node->addChild($child);  
+    }    
+    
+    function getNodesByDepth($depth,$node)
+    {
+        if ($depth == $node->getDepth())
+        {
+            $this->childsByDepth[] = $node;
+        }
+        else
+        {
+            foreach($node->getChildren() as $child)
+            {
+                $this->getNodesByDepth($depth,$child); 
+            }
+        }
+    }
+    
+    function findChild($data,$type,$node)
+    {
+        if ($node->getChild($data,$type))
+        {
+            $this->childFound = $node->getChild($data,$type);
+        }
+        else
+        {
+            foreach ($node->getChildren() as $child)
+            {
+                $this->findChild($data,$type,$child);
+            }
+
+        }
+
+    }
+}
