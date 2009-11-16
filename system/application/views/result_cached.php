23240566a16ebba46e5ea3fc573eff683781e63f
Skander Jabouzi <jabouzi@gmail.com>
2009-11-16 04:00:06 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/3b/trunk@48 1045d7f8-9d8e-4f2c-ac63-a42effcbc9fb

---
+++ system/application/views/result_cached.php
@@ -0,0 +1,237 @@
+    <br>
+    <table border=1 class="table" >
+        <tr>
+                        <th style="background-color:#7F7F7F;" class="th">annonceur</th>
+                        <th style="background-color:#7F7F7F;" class="th">marque</th>
+                        <th style="background-color:#7F7F7F;" class="th">campagne</th>
+                        <th style="background-color:#7F7F7F;" class="th">regie</th>
+                        <th style="background-color:red;" class="thres">Nbre Panneaux</th>
+            <th style="background-color:red;" class="thres">GRP</th>
+        </tr>
+        <tr>
+        <pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'SUNSILK'</font> <i>(length=7)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'CLEAR'</font> <i>(length=5)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'SIGNAL'</font> <i>(length=6)</i>
+</pre>------------------------------<pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'SUNSILK LA VIE N&apos;&apos;ATTEND PAS'</font> <i>(length=28)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'>
+<b>array</b>
+  0 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>22</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'INCONNU'</font> <i>(length=7)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>23</i>]
+              ...
+  1 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>20</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'ODV+'</font> <i>(length=4)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>19</i>]
+              ...
+  2 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>33</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'MEDIA IN'</font> <i>(length=8)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>32</i>]
+              ...
+  3 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>54</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'REGIE 7'</font> <i>(length=7)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>53</i>]
+              ...
+  4 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>61</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'MIP'</font> <i>(length=3)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>60</i>]
+              ...
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'SHAMPOING CLEAR'</font> <i>(length=15)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'>
+<b>array</b>
+  0 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>56</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'ODV+'</font> <i>(length=4)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>55</i>]
+              ...
+  1 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>58</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'MEDIA IN'</font> <i>(length=8)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>50</i>]
+              ...
+  2 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>89</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'REGIE 7'</font> <i>(length=7)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>88</i>]
+              ...
+  3 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>94</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'MIP'</font> <i>(length=3)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>93</i>]
+              ...
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'Christian Dior parfum'</font> <i>(length=21)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'>
+<b>array</b>
+  0 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>85</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'ODV+'</font> <i>(length=4)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>91</i>]
+              ...
+  1 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>84</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'MIP'</font> <i>(length=3)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>86</i>]
+              ...
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'SIGNAL 2'</font> <i>(length=8)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'>
+<b>array</b>
+  0 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>78</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'MEDIA IN'</font> <i>(length=8)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>77</i>]
+              ...
+  1 <font color='#888a85'>=&gt;</font> 
+    <b>object</b>(<i>Node</i>)[<i>80</i>]
+      <i>private</i> 'type' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'regie'</font> <i>(length=5)</i>
+      <i>private</i> 'data' <font color='#888a85'>=&gt;</font> <small>string</small> <font color='#cc0000'>'MIP'</font> <i>(length=3)</i>
+      <i>private</i> 'depth' <font color='#888a85'>=&gt;</font> <small>int</small> <font color='#4e9a06'>3</font>
+      <i>private</i> 'children' <font color='#888a85'>=&gt;</font> 
+        <b>array</b>
+          0 <font color='#888a85'>=&gt;</font> 
+            <b>object</b>(<i>Node</i>)[<i>75</i>]
+              ...
+</pre>------------------------------<pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'INCONNU'</font> <i>(length=7)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'ODV+'</font> <i>(length=4)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'MEDIA IN'</font> <i>(length=8)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'REGIE 7'</font> <i>(length=7)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'MIP'</font> <i>(length=3)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'ODV+'</font> <i>(length=4)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'MEDIA IN'</font> <i>(length=8)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'REGIE 7'</font> <i>(length=7)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'MIP'</font> <i>(length=3)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'ODV+'</font> <i>(length=4)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'MIP'</font> <i>(length=3)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'MEDIA IN'</font> <i>(length=8)</i>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>string</small> <font color='#cc0000'>'MIP'</font> <i>(length=3)</i>
+</pre>------------------------------<pre class='xdebug-var-dump' dir='ltr'><small>int</small> <font color='#4e9a06'>3</font>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>int</small> <font color='#4e9a06'>4</font>
+</pre><pre class='xdebug-var-dump' dir='ltr'><small>int</small> <font color='#4e9a06'>13</font>
+</pre>        <td rowspan="156">UNILEVER</td>
+                <td rowspan="1">SUNSILK</td>
+                <td rowspan="5">SUNSILK LA VIE N''ATTEND PAS</td>
+                <td>INCONNU</td>
+        <td><center>2</center></td>
+        <td><center>68,602</center></td>         
+        </tr>        
+                <td>ODV+</td>
+        <td><center>8</center></td>
+        <td><center>141,265</center></td>         
+        </tr>        
+                <td>MEDIA IN</td>
+        <td><center>20</center></td>
+        <td><center>523,530</center></td>         
+        </tr>        
+                <td>REGIE 7</td>
+        <td><center>12</center></td>
+        <td><center>319,609</center></td>         
+        </tr>        
+                <td>MIP</td>
+        <td><center>4</center></td>
+        <td><center>98,410</center></td>         
+        </tr>        
+                        <tr>        
+                <td rowspan="2">CLEAR</td>
+                <td rowspan="4">SHAMPOING CLEAR</td>
+                <td>ODV+</td>
+        <td><center>4</center></td>
+        <td><center>43,112</center></td>         
+        </tr>        
+                <td>MEDIA IN</td>
+        <td><center>45</center></td>
+        <td><center>1 610,171</center></td>         
+        </tr>        
+                <td>REGIE 7</td>
+        <td><center>6</center></td>
+        <td><center>162,001</center></td>         
+        </tr>        
+                <td>MIP</td>
+        <td><center>3</center></td>
+        <td><center>122,010</center></td>         
+        </tr>        
+                        <td rowspan="2">Christian Dior parfum</td>
+                <td>ODV+</td>
+        <td><center>1</center></td>
+        <td><center>15,384</center></td>         
+        </tr>        
+                <td>MIP</td>
+        <td><center>1</center></td>
+        <td><center>19,971</center></td>         
+        </tr>        
+                        <tr>        
+                <td rowspan="1">SIGNAL</td>
+                <td rowspan="2">SIGNAL 2</td>
+                <td>MEDIA IN</td>
+        <td><center>2</center></td>
+        <td><center>40,838</center></td>         
+        </tr>        
+                <td>MIP</td>
+        <td><center>1</center></td>
+        <td><center>15,046</center></td>         
+        </tr>        
+                        <tr>        
+            </table><br>
+</div>
--- system/application/views/test.php
+++ system/application/views/test.php
@@ -2,17 +2,46 @@
 
 var_dump($panneaux);
 //$this->load->library('tree');
+
 foreach($panneaux as $panneau)
-{
-    var_dump($panneau->getRoot()->getdata());
+{    
+    var_dump(count($panneau->getRoot()->getChildren()));
     foreach($panneau->getRoot()->getChildren() as $child)
     {
         var_dump($child->getData());
+        var_dump(count($child->getChildren()));
         foreach($child->getChildren() as $grandChild)
         {
             var_dump($grandChild->getData());
+            var_dump(count($grandChild->getChildren()));
+            foreach($grandChild->getChildren() as $grandGrandChild)
+            {
+                var_dump($grandGrandChild->getData());
+                var_dump(count($grandGrandChild->getChildren()));
+                foreach($grandGrandChild->getChildren() as $grandGrandGrandChild)
+                {
+                    var_dump($grandGrandGrandChild->getData());
+                    var_dump(count($grandGrandGrandChild->getChildren()));
+                    foreach($grandGrandGrandChild->getChildren() as $grandGrandGrandGrandChild)
+                    {
+                        var_dump($grandGrandGrandGrandChild->getData());
+                    }
+                }
+            }
         }
     }
+    echo "===============================";
 }
+
+//$panneaux[1]->findChild("123","123",$panneaux[1]->getRoot());
+$panneaux[0]->getNodesByDepth(1,$panneaux[0]->getRoot());
+var_dump(count($panneaux[0]->getChildsByDepth()));
+//var_dump($panneau->getRoot()->getdata());
+
+echo "===============================";
+
+//$panneaux[0]->findChild("123","123",$panneaux[0]->getRoot());
+//var_dump($panneau->getRoot()->getdata());
+
 
 
