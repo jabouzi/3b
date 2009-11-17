70cd30cc13e061997d8084edc2648e5fb24dda29
Skander Jabouzi <jabouzi@gmail.com>
2009-11-17 04:37:26 +0000

    ajouter les views

---
+++ system/application/views/result_5.php
@@ -0,0 +1,58 @@
+<?php
+/*Skander Jabouzi 2009 result.php : afficher le tableau croisé et le cacher dans fichier*/
+
+ob_start();
+$cachefile  = "./system/application/views/result_cached.php";  
+$fp = fopen($cachefile, 'w');              
+?>
+<?php  foreach($panneaux as $panneau):?>
+<? if ($panneau->getRoot()->hasChildren()):?>
+    <br>
+    <table border=1 class="table" >
+        <tr>
+            <?php for($key = 0; $key < count($keys); $key++):?>
+            <th style="background-color:#7F7F7F;" class="th"><?php echo $keys[$key];?></th>
+            <?php endfor;?>
+            <th style="background-color:red;" class="thres">Nbre Panneaux</th>
+            <th style="background-color:red;" class="thres">GRP</th>
+        </tr>
+        <tr>
+        <?php            
+            $panneau->getNodesByDepth(4,$panneau->getRoot()); $count = count($panneau->getChildsByDepth());  
+        ?>
+        <td rowspan="<?php echo $count;?>"><?php echo $panneau->getRoot()->getData();?></td>
+        <?foreach($panneau->getRoot()->getChildren() as $child):?>
+        <?$panneau->resetChildsByDepth();?>
+        <?$panneau->getNodesByDepth(4,$child); $count = count($panneau->getChildsByDepth());?>
+        <td rowspan="<?php echo $count;?>"><?php echo $child->getData();?></td>
+        <?foreach($child->getChildren() as $grandChild):?>
+        <?$panneau->resetChildsByDepth();?>
+        <?$panneau->getNodesByDepth(4,$grandChild); $count = count($panneau->getChildsByDepth());?>
+        <td rowspan="<?php echo $count;?>"><?php echo $grandChild->getData();?></td>
+        <?foreach($grandChild->getChildren() as $grandGrandChild):?>
+        <?$panneau->resetChildsByDepth();?>
+        <?$panneau->getNodesByDepth(4,$grandGrandChild); $count = count($panneau->getChildsByDepth());?>
+        <td rowspan="<?php echo $count;?>"><?php echo $grandGrandChild->getData();?></td>
+        <?foreach($grandGrandChild->getChildren() as $grandGrandGrandChild):?>
+        <td><?php echo $grandGrandGrandChild->getData();?></td>
+        <td><center><?php echo $grandGrandGrandChild->getChildAt(0)->getData();?></center></td>
+        <td><center><?php echo number_format($grandGrandGrandChild->getChildAt(0)->getChildAt(0)->getData(),3, ',', ' ');?></center></td>         
+        </tr>        
+        <?php endforeach;?>
+        <?php endforeach;?>
+        <?php endforeach;?>
+        <tr>        
+        <?php endforeach;?>
+    </table><br>
+<?endif;?>
+<?php endforeach;?>
+</div>
+<?
+// save the contents of output buffer to the file
+fwrite($fp, ob_get_contents());
+// close the file
+fclose($fp);
+// Send the output to the browser
+ob_end_flush(); 
+?>
+</div>
